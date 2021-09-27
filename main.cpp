603ed7819e18643645a4ee968e78a80ba2bb78c6
Aleksandr3121 <rbnfqly@gmail.com>
2021-09-27 13:28:56 +0900

    add class Display

--- main.cpp
+++ main.cpp
@@ -7,14 +7,17 @@ struct Coordinate {
     int x = 0, y = 0;
 };
 
+const int HeightDisplay = 50;
+const int WidthDisplay = 80;
+
 class Window {
 public:
     void Move(const Coordinate& move_vector) {
         top_lef_corner.x += move_vector.x;
         top_lef_corner.y += move_vector.y;
-        if (top_lef_corner.x > 79)top_lef_corner.x = 79;
+        if (top_lef_corner.x > WidthDisplay)top_lef_corner.x = WidthDisplay-1;
         else if (top_lef_corner.x < 0)top_lef_corner.x = 0;
-        if (top_lef_corner.y > 49)top_lef_corner.y = 49;
+        if (top_lef_corner.y > HeightDisplay)top_lef_corner.y = HeightDisplay-1;
         else if (top_lef_corner.y < 0)top_lef_corner.y = 0;
         cout << "New coordinate of the window: " << top_lef_corner.x << ' ' << top_lef_corner.y << endl;
     }
@@ -28,26 +31,48 @@ public:
         }
     }
 
-    void Display() const {
-        for (size_t h = 0; h < top_lef_corner.y; ++h) {
-            for (size_t w = 0; w < 80; ++w) {
+    const Coordinate& GetCoordinate() const {
+        return top_lef_corner;
+    }
+
+    const int& GetHeight() const {
+        return height;
+    }
+
+    const int& GetWidth() const {
+        return width;
+    }
+
+private:
+    int height;
+    int width;
+    Coordinate top_lef_corner;
+};
+
+class Display {
+public:
+    Display() : height(HeightDisplay), width(WidthDisplay) {}
+
+    void PrintWindow(const Window& window) const {
+        for (size_t h = 0; h < window.GetCoordinate().y; ++h) {
+            for (size_t w = 0; w < width; ++w) {
                 cout << 0;
             }
             cout << endl;
         }
         cout << setfill('0');
-        for (size_t h = top_lef_corner.y; h < top_lef_corner.y+height; ++h) {
-            cout<<setw(1+top_lef_corner.x);
-            for (size_t w = 0; w < width; ++w) {
-                cout<<1;
+        for (size_t h = window.GetCoordinate().y; h < window.GetCoordinate().y + window.GetHeight(); ++h) {
+            cout << setw(1 + window.GetCoordinate().x);
+            for (size_t w = 0; w < window.GetWidth(); ++w) {
+                cout << 1;
             }
-            for (size_t w = width + top_lef_corner.x; w < 80; ++w) {
+            for (size_t w = window.GetWidth() + window.GetCoordinate().x; w < width; ++w) {
                 cout << 0;
             }
             cout << endl;
         }
-        for(size_t h=top_lef_corner.y+height; h<50; ++h){
-            for (size_t w = 0; w < 80; ++w) {
+        for (size_t h = window.GetCoordinate().y + window.GetHeight(); h < height; ++h) {
+            for (size_t w = 0; w < width; ++w) {
                 cout << 0;
             }
             cout << endl;
@@ -55,13 +80,12 @@ public:
     }
 
 private:
-
-    int height;
-    int width;
-    Coordinate top_lef_corner;
+    const int height;
+    const int width;
 };
 
 int main() {
+    Display display;
     Window window;
     string command;
     Coordinate move_vector;
@@ -74,7 +98,7 @@ int main() {
             cin >> h >> w;
             window.Resize(h, w);
         } else if (command == "display") {
-            window.Display();
+            display.PrintWindow(window);
         } else cout << "Unknown command" << endl;
     }
     return 0;
